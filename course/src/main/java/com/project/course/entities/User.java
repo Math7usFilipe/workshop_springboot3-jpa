7ca5b73c7f3a72e080a5b6caa41a7c376776ab3e
Math7usFilipe <mxtheus829melo@gmail.com>
2024-10-29 13:31:24 -0300

    Service layer, component registration

--- course/src/main/java/com/project/course/entities/User.java
+++ course/src/main/java/com/project/course/entities/User.java
@@ -10,10 +10,9 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
-    //serie number at of interface Serializable
+public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-    //basic atributes
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,47 +20,62 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
-    //constructors
+
+    // Construtor padrão
+    public User() {
+    }
+
+    // Construtor com parâmetros
     public User(Long id, String name, String email, String phone, String password) {
-        super();
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.password = password;
     }
-    //gets and sets
+
+    // Gets e Sets
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    //Hashcode and equals
+
+    // hashCode e equals
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -69,22 +83,15 @@ public class User implements Serializable{
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
+
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
         User other = (User) obj;
         if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+            return other.id == null;
+        } else return id.equals(other.id);
     }
-    
-    
 }
